405ad9156a7dae1f9fe7c5b79d9176a7d326f392
rishika251 <rishikasadineni@gmail.com>
2022-11-16 23:47:33 +0530

    Added:wellcome

---
+++ Wellcome.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.day14;
+
+public class Wellcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Wellcome to linkedlist programs");
+	}
+
+}

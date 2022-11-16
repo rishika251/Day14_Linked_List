5ced57b82c63f5cd19cc8704ad3b9dd9aaec4e84
rishika251 <rishikasadineni@gmail.com>
2022-11-16 23:55:28 +0530

    Added:add first element to linkedlist

---
+++ LinkedListMain.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.day14;
+
+public class LinkedListMain {
+	public static void main(String[] args) {
+
+		LinkedList linkedlist = new LinkedList();
+
+		linkedlist.addFirst(70);
+		linkedlist.display();
+		linkedlist.addFirst(30);
+		linkedlist.display();
+		linkedlist.addFirst(56);
+		linkedlist.display();
+	}
+
+}
+

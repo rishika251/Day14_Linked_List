ed1380d4d2d25d31747d7d398f3296c6fae74c4d
rishika251 <rishikasadineni@gmail.com>
2022-11-17 08:54:17 +0530

    Added:delete the element in between nodes

--- LinkedList.java
+++ LinkedList.java
@@ -162,5 +162,28 @@ public void insertAtSpecificIndex(int userIndex, Object data) {
 	}
 }
 
+
+
+
+public void deleteAtSpecificIndex(int userIndex) {
+	if (userIndex == 0)
+		deleteFirst();
+	else if (userIndex == size())
+		deleteLast();
+	else {
+		int index = 1; // userIndex=1
+		Node left = head;
+		Node right = head.next;
+
+		while (index < userIndex) {
+			left = left.next;
+			right = right.next;
+			index++;
+		}
+
+		left.next = right.next;
+
+	}
+}
 }
 
